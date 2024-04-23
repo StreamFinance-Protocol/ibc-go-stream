0bbf711b670c1472067e58ef406ac7c7763872f0
soletty <109605001+soletty@users.noreply.github.com>
2024-04-23 12:21:50 -0700

    logging

--- go.mod
+++ go.mod
@@ -1,10 +1,11 @@
 go 1.21
 
-module github.com/cosmos/ibc-go/v8
+//module github.com/cosmos/ibc-go/v8
+module github.com/StreamProtocol-Finance/ibc-go-stream
 
-retract (
-	[v8.0.0, v8.1.1] // contains ASA-2024-007 vulnerability
-)
+// retract (
+// 	[v8.0.0, v8.1.1] // contains ASA-2024-007 vulnerability
+// )
 
 require (
 	cosmossdk.io/api v0.7.3
@@ -26,6 +27,7 @@ require (
 	github.com/cosmos/cosmos-sdk v0.50.5
 	github.com/cosmos/gogoproto v1.4.11
 	github.com/cosmos/ibc-go/modules/capability v1.0.0
+	github.com/cosmos/ibc-go/v8 v8.2.0
 	github.com/cosmos/ics23/go v0.10.0
 	github.com/golang/protobuf v1.5.4
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0
--- testing/app.go
+++ testing/app.go
@@ -2,6 +2,7 @@ package ibctesting
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 	"time"
 
@@ -120,6 +121,10 @@ func SetupWithGenesisValSet(tb testing.TB, valSet *tmtypes.ValidatorSet, genAccs
 
 	// update total supply
 	bankGenesis := banktypes.NewGenesisState(banktypes.DefaultGenesisState().Params, balances, sdk.NewCoins(), []banktypes.Metadata{}, []banktypes.SendEnabled{})
+
+	fmt.Printf("XXXXXXXXXX")
+	bankGenesis.Balances = append(bankGenesis.Balances, banktypes.Balance{Address: "dydx1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8wx2cfg", Coins: sdk.NewCoins(sdk.NewInt64Coin("stake", 5000))})
+
 	genesisState[banktypes.ModuleName] = app.AppCodec().MustMarshalJSON(bankGenesis)
 
 	stateBytes, err := json.MarshalIndent(genesisState, "", " ")
